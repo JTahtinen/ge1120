1e4a271f9f10d2833111f3d3faafd5beebfe3833
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2020-11-24 11:26:00 +0200

    changed build file settings

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-cl /Zi /DDEBUG /DSDL_MAIN_HANDLED src/*.cpp src/system/*.cpp src/game/*.cpp src/graphics/*.cpp src/math/*.cpp /Iinclude/ lib/*.lib OpenGL32.lib /Foobj/ /Febin/Ge1120.exe	
+@ECHO OFF
+cl /Zi /DDEBUG /DSDL_MAIN_HANDLED /EHsc src/*.cpp src/system/*.cpp src/game/*.cpp src/graphics/*.cpp src/math/*.cpp /Iinclude/ lib/*.lib OpenGL32.lib /Foobj/ /Febin/Ge1120.exe	
