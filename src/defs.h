ffbc76ae78d833318d6a114120c0985a50c2abf4
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2021-03-02 18:26:57 +0200

    Turned variable arguments implementation into a macro

--- src/defs.h
+++ src/defs.h
@@ -1,89 +1,74 @@
 #pragma once
 #include <GL/glew.h>
-#include <iostream>
 #include <stdarg.h>
+#include <windows.h>
+#include <stdio.h>
 
+#define VA_ARGS_IMPLEMENTATION \
+    va_list ap; \
+    va_start(ap, content); \
+    char* sval; \
+    int ival; \
+    double dval; \
+    char* p; \
+    for (p = content; *p; ++p) \
+    { \
+        if (*p != '%') \
+        { \
+            putchar(*p); \
+            continue; \
+        } \
+        switch (*++p) \
+        { \
+            case 'd': \
+                ival = va_arg(ap, int); \
+                printf("%d", ival); \
+                break; \
+            case 'f': \
+                dval = va_arg(ap, double); \
+                printf("%f", dval); \
+                break; \
+            case 's': \
+                for (sval = va_arg(ap, char*); *sval; ++sval) \
+                { \
+                    putchar(*sval); \
+                } \
+                break; \
+            default: \
+                putchar(*p); \
+                break; \
+        } \
+    } \
+    va_end(ap);
 
 inline void message(char* content, ...)
 {
-    va_list ap;
-
-    va_start(ap, content);
-    char* sval;
-    int ival;
-    double dval;
-    char* p;
-
-    for (p = content; *p; ++p)
-    {
-        if (*p != '%')
-        {
-            putchar(*p);
-            continue;
-        }
-        switch (*++p)
-        {
-            case 'd':
-                ival = va_arg(ap, int);
-                printf("%d", ival);
-                break;
-            case 'f':
-                dval = va_arg(ap, double);
-                printf("%f", dval);
-                break;
-            case 's':
-                for (sval = va_arg(ap, char*); *sval; ++sval)
-                {
-                    putchar(*sval);
-                }
-                break;
-            default:
-                putchar(*p);
-                break;
-        }
-    }
-    va_end(ap);
+    VA_ARGS_IMPLEMENTATION;
 }
 
 inline void err(char* content, ...)
 {
-    va_list ap;
-
-    va_start(ap, content);
-    char* sval;
-    int ival;
-    double dval;
-    char* p;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole,
+                            FOREGROUND_RED);  
     printf("[ERROR] ");
-    for (p = content; *p; ++p)
-    {
-        if (*p != '%')
-        {
-            putchar(*p);
-            continue;
-        }
-        switch (*++p)
-        {
-            case 'd':
-                ival = va_arg(ap, int);
-                printf("%d", ival);
-                break;
-            case 'f':
-                dval = va_arg(ap, double);
-                printf("%f", dval);
-                break;
-            case 's':
-                for (sval = va_arg(ap, char*); *sval; ++sval)
-                {
-                    putchar(*sval);                 
-                }
-                break;
-            default:
-                putchar(*p);
-                break;
-        }
-    }
-    va_end(ap);
+  
+    SetConsoleTextAttribute(hConsole, 
+                            FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+    VA_ARGS_IMPLEMENTATION;
+}
+
+inline void warn(char* content, ...)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole,
+                            FOREGROUND_RED | FOREGROUND_GREEN);  
+    printf("[WARNING] ");
+    SetConsoleTextAttribute(hConsole, 
+                            FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+    VA_ARGS_IMPLEMENTATION;
 }
 
 
@@ -97,7 +82,7 @@ inline bool GLLogCall()
 {
     while (GLenum error = glGetError())
     {
-        std::cout << "[OpenGL ERROR] (" << error << ")" << std::endl;
+        message("[OpenGL ERROR] (%d)\n", error);
         return false;
     }
     return true;
