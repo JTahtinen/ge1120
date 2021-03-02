1c63f8a94af257fc17e3efa1e0797d9af0390c15
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2021-03-02 18:28:43 +0200

    Added binary file loading and saving

--- src/util/file.h
+++ src/util/file.h
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
+#include <stdio.h>
+#include "../defs.h"
 
 inline std::string loadTextFile(const std::string &filepath)
 {
@@ -25,4 +26,38 @@ inline std::string loadTextFile(const std::string &filepath)
         file.close();
     }
     return text;
+}
+
+inline void loadBinaryFile(const char* filepath, void* ptr, size_t numBytes)
+{
+    FILE* fp = fopen(filepath, "rb");
+    if (!fp)
+    {
+        err("Could not open binary file: %s\n", filepath);
+        return;
+    }
+    //Determine size of file
+    fseek(fp, 0, SEEK_END); 
+    size_t fileSize = ftell(fp); 
+    fseek(fp, 0, SEEK_SET);
+
+    size_t bytesToRead;
+    if (numBytes > fileSize)
+    {
+        warn("Tried to read bytes past end of file: %s\n", filepath); 
+        bytesToRead = fileSize;
+    }
+    else
+    {
+        bytesToRead = numBytes;
+    }
+    fread(ptr, 1, bytesToRead, fp);
+    fclose(fp);
+}
+
+inline void saveBinaryFile(const char* filepath, void* data, size_t numBytes)
+{
+    FILE* fp = fopen(filepath, "wb");
+    fwrite(data, numBytes, 1, fp);  
+    fclose(fp);
 }
