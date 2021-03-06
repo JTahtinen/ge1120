f84da90f9d7696941c35df6636a7fb6b288c0f67
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2021-03-06 21:20:54 +0200

    added ui folder to build

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-cl /Zi /DDEBUG /DSDL_MAIN_HANDLED /EHsc src/*.cpp src/system/*.cpp src/game/*.cpp src/graphics/*.cpp src/math/*.cpp /Iinclude/ lib/*.lib OpenGL32.lib /Foobj/ /Febin/Ge1120.exe	
+cl /Zi /DDEBUG /Dno-strict-aliasing /DSDL_MAIN_HANDLED /EHsc src/*.cpp src/system/*.cpp src/game/*.cpp src/graphics/*.cpp src/math/*.cpp src/ui/*.cpp /Iinclude/ lib/*.lib OpenGL32.lib /Foobj/ /Febin/Ge1120.exe	
